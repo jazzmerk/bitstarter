e143758d601d1c4089b28b9a280af34d95a1b7c9
jazzmerk <jnmerk@gmail.com>
2013-07-11 16:06:52 +0000

    added require fs, still printing teststring

--- web.js
+++ web.js
@@ -2,15 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var readstring = new Buffer(30);
+fs = require ('fs');
+var readfile = new Buffer(30);
 var teststring = "teststring";
-readstring = fs.readFileSync('index.html');
-readstring = buf.toString(readstring);
+var readfile = fs.readFileSync('index.html');
+var readstring = toString(readfile);
 app.get('/', function(request, response) {
   response.send(teststring);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("listening on " + port);
 });
