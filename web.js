52b06d3831f0ce38cf3a680544eb18034140d7d6
jazzmerk <jnmerk@gmail.com>
2013-07-11 16:25:10 +0000

    put tostring command in send argument

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 fs = require ('fs');
-var readfile = new Buffer(30);
-var teststring = "teststring";
+var readfile = new Buffer(40);
 var readfile = fs.readFileSync('index.html');
-var readstring = toString(readfile);
+//var readstring = toString(readfile);
 app.get('/', function(request, response) {
-  response.send(readfile);
+  response.send(toString(readfile));
 });
 
 var port = process.env.PORT || 5000;
