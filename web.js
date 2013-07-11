9c8bee392980874a370a4a0325deff9fab420838
jazzmerk <jnmerk@gmail.com>
2013-07-11 17:06:39 +0000

    Think I've got damn syntax of tostring

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 fs = require ('fs');
 var readfile = new Buffer(40);
 var readfile = fs.readFileSync('index.html');
-//var readstring = toString(readfile);
+var readstring = toString(readfile);
 app.get('/', function(request, response) {
-  response.send(readfile);
+  response.send(readstring);
 });
 
 var port = process.env.PORT || 5000;
